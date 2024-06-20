c8ef19e6ba1961190d81cd71a874b97af11b3f77
Marcos Garcia de Lima <marckfree@gmail.com>
2024-06-20 00:46:18 -0300

    Solve `d_control_flow_strings` exercise

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -20,9 +20,15 @@ fn main() {
         // - If arg is "double", then call the double() function
         // - If arg is anything else, then call the count() function, passing "arg" to it.
 
-
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
         // after "cargo run".  For example "cargo run sum"
+        if arg == "sum" {
+            sum();
+        } else if arg == "double" {
+            double();
+        } else {
+            count(arg);
+        }
     }
 }
 
@@ -32,6 +38,9 @@ fn sum() {
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
 
+    for i in 7..=23 {
+        sum += i;
+    }
 
     println!("The sum is {}", sum);
 }
@@ -43,8 +52,15 @@ fn double() {
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
 
+    while x < 500 {
+        count += 1;
+        x *= 2;
+    }
 
-    println!("You can double x {} times until x is larger than 500", count);
+    println!(
+        "You can double x {} times until x is larger than 500",
+        count
+    );
 }
 
 fn count(arg: String) {
@@ -52,7 +68,16 @@ fn count(arg: String) {
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
     // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+    let mut count = 0;
 
+    loop {
+        count += 1;
 
-    println!(); // This will output just a newline at the end for cleanliness.
+        print!("{} ", arg);
+        println!(); // This will output just a newline at the end for cleanliness.
+
+        if count == 8 {
+            break;
+        }
+    }
 }
