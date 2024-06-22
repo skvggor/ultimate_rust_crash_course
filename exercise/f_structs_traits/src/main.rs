14a32369ea266ee84258c5a70af9825f0c928281
Marcos Garcia de Lima <marckfree@gmail.com>
2024-06-22 15:52:00 -0300

    Solve `f_structs_traits` exercise

--- exercise/f_structs_traits/src/main.rs
+++ exercise/f_structs_traits/src/main.rs
@@ -6,7 +6,6 @@
 //
 //  trait Bite...
 
-
 // 2. Now create a struct named Grapes with a field that tracks how many grapes are left.  If you
 // need a hint, look at how it was done for Carrot at the bottom of this file (you should probably
 // use a different field, though).
@@ -14,25 +13,29 @@
 // #[derive(Debug)] // include this line right before your struct definition
 // struct Grapes...
 
-
 // 3. Implement Bite for Grapes.  When you bite a Grapes, subtract 1 from how many grapes are left.
 // If you need a hint, look at how it was done for Carrot at the bottom of this file.
 //
 // impl Bite for...
 
-
 fn main() {
     // Once you finish #1 above, this part should work.
-    let mut carrot = Carrot { percent_left: 100.0 };
+    let mut carrot = Carrot {
+        percent_left: 100.0,
+    };
+
     carrot.bite();
+
     println!("I take a bite: {:?}", carrot);
 
     // 4. Uncomment and adjust the code below to match how you defined your
     // Grapes struct.
     //
-    //let mut grapes = Grapes { amount_left: 100 };
-    //grapes.bite();
-    //println!("Eat a grape: {:?}", grapes);
+    let mut grapes = Grapes { amount_left: 100 };
+
+    grapes.bite();
+
+    println!("Eat a grape: {:?}", grapes);
 
     // Challenge: Uncomment the code below. Create a generic `bunny_nibbles`
     // function that:
@@ -41,8 +44,29 @@ fn main() {
     // Hint: Define the generic type between the function name and open paren:
     //       fn function_name<T: Bite>(...)
     //
-    //bunny_nibbles(&mut carrot);
-    //println!("Bunny nibbles for awhile: {:?}", carrot);
+    bunny_nibbles(&mut carrot);
+    println!("Bunny nibbles for awhile: {:?}", carrot);
+
+    fn bunny_nibbles<T: Bite>(item: &mut T) {
+        for i in 0..9 {
+            item.bite();
+        }
+    }
+}
+
+trait Bite {
+    fn bite(self: &mut Self);
+}
+
+#[derive(Debug)]
+struct Grapes {
+    amount_left: i32,
+}
+
+impl Bite for Grapes {
+    fn bite(&mut self) {
+        self.amount_left -= 1
+    }
 }
 
 #[derive(Debug)] // This enables using the debugging format string "{:?}"
